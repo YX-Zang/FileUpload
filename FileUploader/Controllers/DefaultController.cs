ba12d1a8f425117e9debd4e66fe00e9c97276d26
YX-Zang <34359549+YX-Zang@users.noreply.github.com>
2018-08-21 22:52:06 +0800

    UploadFile
    upoad excel data to sql db MVC ASP

---
+++ FileUploader/Controllers/DefaultController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+namespace FileUploader.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Upload(HttpPostedFileWrapper uploadFile)
+        {
+            if (uploadFile != null)
+            {
+                uploadFile.SaveAs(@"c:\upload-file\" + Path.GetFileName(uploadFile.FileName));
+            }
+            return View();
+        }
+    }
+}
